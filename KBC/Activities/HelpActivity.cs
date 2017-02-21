3e1cd680f21131e6daa13745de5de0090577f6c9
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-02-21 13:19:54 +0530

    Update Help View

--- KBC/Activities/HelpActivity.cs
+++ KBC/Activities/HelpActivity.cs
@@ -29,26 +29,26 @@ There is a 30 second timer till the first safe level and 60 second till the seco
 ";
 
             var audiencePollText = FindViewById<TextView>(Resource.Id.audiencePollText);
-            audiencePollText.Text = @"
-1. Audience Poll
+            audiencePollText.Text = @"1. Audience Poll
+
 This will simulate a poll amongst the audience attending KBC and will show you the results of the poll in the form of a bar graph.
 ";
 
             var fifty50Text = FindViewById<TextView>(Resource.Id.fifty50Text);
-            fifty50Text.Text = @"
-2. Fifty Fifty
+            fifty50Text.Text = @"2. Fifty Fifty
+
 This lifeline will remove 2 wrong choices and you have to pick the right answer from the remaining 2 choices.
 ";
 
             var doubleDipText = FindViewById<TextView>(Resource.Id.doubleDipText);
-            doubleDipText.Text = @"
-3. Double Dip
+            doubleDipText.Text = @"3. Double Dip
+
 This lifeline allows you to answer two options for a question, winning if either of those is correct.
 ";
 
             var changeQuestionText = FindViewById<TextView>(Resource.Id.changeQuestionText);
-            changeQuestionText.Text = @"
-4. Flip the Question
+            changeQuestionText.Text = @"4. Flip the Question
+
 This will skip the current questions and show you a new question without increasing your money.
 ";
         }
